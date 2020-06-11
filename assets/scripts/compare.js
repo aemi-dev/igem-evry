280d78fa5e9f4f81b597791de896540b5c3315d8
originecode <originecode@users.noreply.github.com>
2020-06-11 18:17:18 +0200

    Add Touch Event Compatibility to Image Comparison - Fix #2

--- assets/scripts/compare.js
+++ assets/scripts/compare.js
@@ -30,14 +30,19 @@ class Compare {
 
     static attachEvent(element) {
         if (element instanceof HTMLElement) {
-            for (const type of ['mousemove', 'touchmove']) {
-                element.addEventListener(type, event => {
-                    inhibitEvent(event);
-                    requestFrame(() => {
-                        element.children[0].style.width = `${event.clientX}px`;
-                    });
-                });   
-            }
+            element.addEventListener('mousemove', event => {
+                requestFrame(() => {
+                    element.children[0].style.width = `${event.clientX}px`;
+                });
+            }); 
+            element.addEventListener('touchmove', event => {
+                inhibitEvent(event);
+                wait.delay(() => requestFrame(() => {
+                    if (event.touches.length > 0) {
+                        element.children[0].style.width = `${event.touches[0].clientX}px`;
+                    }
+                }));
+            });
         }
     }
 }
